fa48110e9fda87ffb331114cd00cd394c565c64c
Paulius Savickas <pauliussavickas18@gmail.com>
2023-04-24 10:53:37 +0300

    added express

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ require('dotenv').config();
 
 const connectDB = require('./config/db');
 connectDB();
+
+const express = require('express'); 
+const app = express(); 
+app.use(express.json());
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on PORT ${process.env.PORT}`) 
+})
