5a076198518ba58ecc130447cfdc46ed3746e067
Darius <darius.draksas244@gmail.com>
2023-04-26 10:32:09 +0300

    added userLogin function

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
     registerUser,
+    loginUser
 } = require('../controllers/UserController');
 
 
 router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
 
 
 module.exports = router;
